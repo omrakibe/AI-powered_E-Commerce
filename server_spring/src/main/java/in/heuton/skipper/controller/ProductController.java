fd0bdbf195ec7927ca20aafd30b3091efff1f7f5
omrakibe <rakibeom30@gmail.com>
2026-05-05 18:06:53 +0530

    added getProduct method and created ProductController class to use RestController and rest of the part im going to continue tomarrow

---
+++ server_spring/src/main/java/in/heuton/skipper/controller/ProductController.java
@@ -0,0 +1,9 @@
+package in.heuton.skipper.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController
+{
+    
+}
--- server_spring/src/main/java/in/heuton/skipper/service/ProductService.java
+++ server_spring/src/main/java/in/heuton/skipper/service/ProductService.java
@@ -3,6 +3,7 @@ package in.heuton.skipper.service;
 import in.heuton.skipper.dto.ProductRequestDTO;
 import in.heuton.skipper.dto.ProductResponseDTO;
 import in.heuton.skipper.entity.Product;
+import in.heuton.skipper.exception.ProductNotFoundException;
 import in.heuton.skipper.repository.IProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class ProductService implements IProductService
     public List<ProductResponseDTO> getAllProducts()
     {
         List<Product> products = prodRepo.findAll();
+
+        if (products.isEmpty())
+        {
+            throw new ProductNotFoundException("No products found");
+        }
+
         return products.stream()
                 .map(product ->
                 {
@@ -42,6 +49,13 @@ public class ProductService implements IProductService
     @Override
     public ProductResponseDTO getProduct(Long id)
     {
-        return null;
+        Product product = prodRepo.findById(id).orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
+
+        ProductResponseDTO response = modelMapper.map(product, ProductResponseDTO.class);
+
+        response.setImageUrl("http://localhost:8080/products/"
+                + product.getId() + "/image");
+
+        return response;
     }
 }
