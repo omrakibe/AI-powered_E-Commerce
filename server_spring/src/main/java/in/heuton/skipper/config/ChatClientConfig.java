ff82a8bb223cb7ef73e0afaa161a7d6900cf3ac6
omrakibe <rakibeom30@gmail.com>
2026-07-05 13:53:20 +0530

    configuration for pgvector is done

---
+++ server_spring/src/main/java/in/heuton/skipper/config/ChatClientConfig.java
@@ -0,0 +1,18 @@
+package in.heuton.skipper.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig
+{
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder)
+    {
+        return builder.build();
+    }
+
+
+}
