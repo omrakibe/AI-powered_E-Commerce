3c2a7d32806d18429b6103e624c9e967741fc6a2
omrakibe <rakibeom30@gmail.com>
2026-07-06 13:09:10 +0530

    Project is completely ready

--- client_react/src/components/Navbar.jsx
+++ client_react/src/components/Navbar.jsx
@@ -134,8 +134,8 @@ const Navbar = ({ onSelectCategory }) => {
   return (
     <nav className="navbar navbar-expand-lg fixed-top bg-white shadow-sm" ref={navbarRef}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="https://telusko.com/">
-          Telusko
+        <a className="navbar-brand" href="/">
+          Skipper
         </a>
         <button
           className="navbar-toggler"
